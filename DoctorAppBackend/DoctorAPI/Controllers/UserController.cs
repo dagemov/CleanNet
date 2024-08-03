bb702f95e8670151e128f909c293acba3698903c
dagemov <sebcoss1234@gmail.com>
2024-08-03 10:18:39 -0400

    init appi with entityFrameworkcore and first entity with controller

---
+++ DoctorAppBackend/DoctorAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace DoctorAPI.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+            return Ok(user);
+        }
+    }
+}
