c04a9b95819f7cfd95d401c33e239f2b8f63eba4
dagemov <sebcoss1234@gmail.com>
2024-08-09 12:02:08 -0400

    created controller erros

---
+++ DoctorAppBackend/DoctorAPI/Controllers/ErrorTestController.cs
@@ -0,0 +1,51 @@
+﻿using Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+
+namespace DoctorAPI.Controllers
+{
+    public class ErrorTestController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ErrorTestController(DataContext context)
+        {
+           _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetNotAuthorizew()
+        {
+            return "You no are Authorized";
+        }
+
+
+        [HttpGet("not-found")]
+        public ActionResult<User> GetNotFound()
+        {
+            var user = _context.Users.Find(-1);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var user = _context.Users.Find(-1);
+            var userString = user.ToString();
+            return userString;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {          
+            return BadRequest("Invalid required - Bad Request");
+        }
+    }
+}
