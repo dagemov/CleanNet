b1755565d0c9d8fa7d47133a9c0dac141f07bf3d
dagemov <sebcoss1234@gmail.com>
2024-08-17 07:50:51 -0400

    added filter to speciality tables

--- DoctorAppBackend/DoctorAppFrontend/src/app/speciality/pages/speciality-list/speciality-list.component.ts
+++ DoctorAppBackend/DoctorAppFrontend/src/app/speciality/pages/speciality-list/speciality-list.component.ts
@@ -128,4 +128,14 @@ export class SpecialityListComponent implements OnInit, AfterViewInit{
       }
     });
   }
+  applyFilter(event: Event)
+  {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator)
+    {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
